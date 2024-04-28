83d419de37e9b3cc36357e60674e12f2ff3dbbd1
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-28 17:28:19 +0530

    structuring Collection dto structuring

--- backend/src/main/java/edu/clothify/dto/CollectionDto.java
+++ backend/src/main/java/edu/clothify/dto/CollectionDto.java
@@ -1,4 +1,22 @@
 package edu.clothify.dto;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class CollectionDto {
+    @Valid
+    private Long id;
+    @NotBlank(message = "category name is null")
+    @Size(min = 2,message = "Name is too short try again")
+    private String name;
 }
