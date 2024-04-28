3a1aa6177305b9454232b3fb13c05fec893460cb
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-28 17:02:39 +0530

    structuring Stock dto

---
+++ backend/src/main/java/edu/clothify/entity/Stock.java
@@ -0,0 +1,46 @@
+package edu.clothify.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "stock")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Product product;
+
+    @OneToOne(mappedBy = "stockId")
+    @JsonIgnore
+    private Cart cart;
+
+    @Column(name = "Color")
+    private String color;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "quantity")
+    private int qty;
+
+    public Stock(Long id) {
+        this.id = id;
+    }
+
+}
