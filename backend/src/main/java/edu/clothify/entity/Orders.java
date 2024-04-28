fcd032102f37a61567f8b6dd8a91d65c550a6d6b
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-28 17:00:23 +0530

    structuring Orders dto

---
+++ backend/src/main/java/edu/clothify/entity/Orders.java
@@ -0,0 +1,64 @@
+package edu.clothify.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import edu.clothify.util.converter.OrderStatusConverter;
+import edu.clothify.util.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "orders")
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @OneToOne
+    @JoinColumn(name = "cart_id")
+    @JsonIgnore
+    private Cart cart;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name ="Contact")
+    private String phone;
+
+    @Column(name ="Tax")
+    private Double tax;
+
+    @Column(name ="Delivery Charge")
+    private Double charge;
+
+    @Column(name = "Land Code")
+    private String zipCode;
+
+    @Column(name = "Order Total")
+    private Double tot;
+
+    @Column(name = "City")
+    private String city;
+
+    @OneToOne(mappedBy = "orders")
+    @JsonIgnore
+    private BillingInfo billingInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @JsonIgnore
+    private Customer customer;
+
+    @Convert(converter =  OrderStatusConverter.class)
+    private OrderStatus status;
+
+
+}
