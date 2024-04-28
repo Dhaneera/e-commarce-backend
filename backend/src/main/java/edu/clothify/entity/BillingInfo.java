27ee992e0ef3cc9b0017898f7cc4a65ee6589957
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-28 17:02:17 +0530

    structuring Cart and BillInfo dtos

---
+++ backend/src/main/java/edu/clothify/entity/BillingInfo.java
@@ -0,0 +1,35 @@
+package edu.clothify.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "billing_info")
+public class BillingInfo{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "orders_id")
+    @JsonIgnore
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @JsonIgnore
+    private Customer customer;
+
+    @Column(name = "Address",nullable = false)
+    private String address;
+
+    @Column(name ="Contact",nullable = false)
+    private String phone;
+}
---
+++ backend/src/main/java/edu/clothify/entity/Cart.java
@@ -0,0 +1,57 @@
+package edu.clothify.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "stock_id")
+    @JsonIgnore
+    private Stock stockId;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customerId;
+
+    @Column(name = "Quantity")
+    private int qty;
+
+    @Column(name = "Product Total")
+    private Double productTot;
+
+    @OneToOne(mappedBy = "cart")
+    private Orders orders;
+
+    private Boolean completed;
+
+    public Cart(Long id) { // Add the missing constructor
+        this.id = id;
+    }
+
+
+    public Cart(Long id, Stock stockId, int qty, boolean completed, Double productTot) {
+        this.id = id;
+        this.qty = qty;
+        this.completed = completed;
+        this.stockId = stockId;
+        this.productTot = productTot;
+    }
+
+    public Cart(long id, boolean completed) {
+        this.id=id;
+        this.completed=completed;
+    }
+}
