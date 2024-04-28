5cac014d16a63b5b3ea4b4e297817f988cf44d92
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-28 16:59:37 +0530

    structuring Customer dto

---
+++ backend/src/main/java/edu/clothify/entity/Customer.java
@@ -0,0 +1,53 @@
+package edu.clothify.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Email")
+    private String mail;
+
+    @OneToOne(mappedBy = "customerId")
+    @JsonIgnore
+    private Cart cart;
+
+    @Column(name = "UserName")
+    private String name ;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name ="Contact")
+    private String phone;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Orders> orders;
+
+    @OneToMany(mappedBy = "customer")
+    private List<BillingInfo> billingInfo;
+
+    public Customer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Customer(String customerName) {
+        this.name =customerName;
+    }
+}
