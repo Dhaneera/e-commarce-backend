9ae89ecfa95664ca7821a7d17c4d30536d334f42
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-05-03 12:32:42 +0530

    cart complete !!

--- backend/src/main/java/edu/clothify/controller/CartController.java
+++ backend/src/main/java/edu/clothify/controller/CartController.java
@@ -1,4 +1,61 @@
 package edu.clothify.controller;
 
+import edu.clothify.dto.CartDto;
+import edu.clothify.entity.Cart;
+import edu.clothify.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/cart")
 public class CartController {
+    @Autowired
+    private CartService cartService;
+    @PostMapping("/add")
+    public boolean addCart(@RequestBody CartDto cartDto){
+        return cartService.addCart(cartDto);
+    }
+
+    @GetMapping("/getAll")
+    public List<CartDto> getAllCartDetails(){
+        return cartService.getAllCartDetails();
+    }
+
+    @PutMapping("/update/{id}")
+    public Cart updateCart(@PathVariable Long id, @RequestBody CartDto cartDto) {
+        return cartService.upadateCart(id,cartDto);
+    }
+
+    @PutMapping("/delete/{id}")
+    public Boolean updateStatus(@PathVariable long id){
+        return cartService.updateStatus(id);
+    }
+
+    @GetMapping("/get/{id}")
+    public CartDto getCartById(@PathVariable long id){
+        return cartService.getCartById(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> error(MethodArgumentNotValidException exception){
+        Map<String, String> map=new HashMap<>();
+        List<ObjectError> list=exception.getBindingResult().getAllErrors();
+        for(ObjectError item:list) {
+            FieldError fieldError=(FieldError) item;
+            String fieldName= fieldError.getField();
+            String message = item.getDefaultMessage();
+            map.put(fieldName,message);
+        }
+        return map;
+    }
 }
